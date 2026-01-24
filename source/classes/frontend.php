f8a333729106c75bc3c00efc092a522c9fd66230
Ivan Nikitin <ivan@nikitin.biz>
2026-01-24 11:29:15 +0300

    feat: Внедрение Composer, npm и PHPUnit
    Полная переработка структуры проекта для внедрения современных практик разработки.
    
    - **Composer:** Добавлена поддержка Composer для автозагрузки классов по стандарту PSR-4. Все классы переведены на новое пространство имен \InEmployeeReports\. Установлены \PHPUnit\ для тестирования и \PHP_CodeSniffer\ для контроля стандартов кода.
    - **npm:** Добавлен \package.json\ для управления frontend-зависимостями. \handsontable\ теперь устанавливается через npm, старая папка удалена. Настроен build-скрипт для копирования ассетов в папку \dist\.
    - **Тесты:** Настроена среда для \PHPUnit\, создан базовый тест для проверки автозагрузки.
    - **Стандарты кода:** Добавлена и настроена проверка по стандартам WordPress. Большая часть кода автоматически отформатирована.

--- source/classes/frontend.php
+++ source/classes/frontend.php
@@ -5,130 +5,124 @@
  * Демо таблиц
  * http://ivannikitin.ivan.wp-server.ru/wp-content/plugins/in-employee-reports/assets/handsontable/
  */
-namespace INER;
-class Frontend extends Base
-{
+namespace InEmployeeReports;
+
+class Frontend extends Base {
+
 	/**
 	 * Шорткод
-	 */	
+	 */
 	const SHORTCODE = 'in_employee_reports';
-	
+
 	/**
 	 * Конструктор
-	 * 
-	 * @param INER\Plugin	$plugin Ссылка на основной объект плагина 
+	 *
+	 * @param Plugin $plugin Ссылка на основной объект плагина
 	 */
-	public function __construct( $plugin )
-	{
+	public function __construct( $plugin ) {
 		// Родительский конструктор
 		parent::__construct( $plugin );
 
 		// Загрузка активов (CSS, JS) на фронтэнде
 		add_action( 'wp_enqueue_scripts', array( $this, 'loadAssets' ) );
-		
+
 		// Регистрация шорткода
 		add_shortcode( self::SHORTCODE, array( $this, 'getHTML' ) );
-		
+
 	}
-	
+
 	/**
 	 * Загрузка CSS и JS
-	 */	
-    public function loadAssets()
-    {	
+	 */
+	public function loadAssets() {
 		// Загрузка только для зарегистрированных пользователей
-		if ( ! is_user_logged_in() ) 
+		if ( ! is_user_logged_in() ) {
 			return;
-		
+		}
+
 		// Регистрация CSS
-		wp_register_style( 'handsontable', $this->plugin->url . 'assets/handsontable/dist/handsontable.full.min.css', array(), '0.34.4');
-		wp_register_style( 'in-employee-reports-frontend', $this->plugin->url . 'assets/css/frontend.css', array( 'wp-jquery-ui-dialog' ), '2.1');
+		wp_register_style( 'handsontable', $this->plugin->url . 'assets/dist/handsontable.full.min.css', array(), '14.4.0' );
+		wp_register_style( 'in-employee-reports-frontend', $this->plugin->url . 'assets/css/frontend.css', array( 'wp-jquery-ui-dialog' ), '2.1' );
 
 		// Загрузка CSS перенесена в метод getHTML, чтобы не грузить их на всех страницах
 
 		// Регистрация скриптов
-		wp_register_script( 'handsontable', $this->plugin->url . 'assets/handsontable/dist/handsontable.full.min.js', array( 'jquery' ), '0.34.4', true );
-		wp_register_script( 'numbro-ru', $this->plugin->url . 'assets/handsontable/dist/numbro/languages/ru-RU.min.js', array( 'handsontable' ), '0.34.4', true );
+		wp_register_script( 'handsontable', $this->plugin->url . 'assets/dist/handsontable.full.min.js', array( 'jquery' ), '14.4.0', true );
+		wp_register_script( 'numbro-ru', $this->plugin->url . 'assets/dist/ru-RU.min.js', array( 'handsontable' ), '14.4.0', true );
 		wp_register_script( 'in-employee-reports', $this->plugin->url . 'assets/js/frontend.js', array( 'jquery', 'jquery-ui-dialog', 'handsontable', 'numbro-ru' ), '2.0', true );
 
-		
 		// Список пользователей для показа в списке
-		$employees = array();	
-		if ( current_user_can( 'administrator' ) ) 
-		{
+		$employees = array();
+		if ( current_user_can( 'administrator' ) ) {
 			// Все роли плагина
 			$allRoles = array_keys( RoleManager::$roles );
 			// Добавим админа
 			$allRoles[] = 'administrator';
 			// Для администратора выбираем всех сотрудгников и подрядчиков
-			$user_query = new \WP_User_Query( array( 'role__in' => $allRoles ) );
+			$user_query   = new \WP_User_Query( array( 'role__in' => $allRoles ) );
 			$employees[0] = '_Все_';
-		}
-		else
-		{
+		} else {
 			// Для обычных пользователей берем данные из RoleManager
 			$user_query = new \WP_User_Query( array( 'include' => RoleManager::getAllowedUsers( get_current_user_id() ) ) );
 		}
-		
-		if ( ! empty( $user_query->results ) ) 
-		{
-			foreach ( $user_query->results as $user ) 
-				$employees[ $user->ID ] = $user->display_name; 
+
+		if ( ! empty( $user_query->results ) ) {
+			foreach ( $user_query->results as $user ) {
+				$employees[ $user->ID ] = $user->display_name;
+			}
 		}
-		
+
 		// Сортируем массив по имени здесь невозможна, поскольку в JS нет ассоциалитвных массивов, а после десериализации
 		// этот массив становится объектом, отсортированным по ключу. Сотрдировка должна быть на frontend'е
 		// asort( $employees );
-		
-		
+
 		// Спиок проектов для автозаполнения
 		$projectList = apply_filters( 'iner_projects', array( 'Оклад', 'Координация проектов' ), get_current_user_id() );
 		sort( $projectList );
-			
+
 		// Данные для скрипта
 		$innerREST = array(
-			'debug'			=> WP_DEBUG,
-			'root'			=> esc_url_raw( rest_url() ),
-			'nonce'			=> wp_create_nonce( 'wp_rest' ),
-			'currentUserId'	=> get_current_user_id(),
-			'employees' 	=> $employees,
-			'projects'		=> $projectList,
+			'debug'         => WP_DEBUG,
+			'root'          => esc_url_raw( rest_url() ),
+			'nonce'         => wp_create_nonce( 'wp_rest' ),
+			'currentUserId' => get_current_user_id(),
+			'employees'     => $employees,
+			'projects'      => $projectList,
 		);
 		wp_localize_script( 'in-employee-reports', 'innerREST', $innerREST );
 
 		// Загрузка скриптов перенесена в метод getHTML, чтобы не грузить их на всех страницах и не было ошибок отсуствия контейнера с таблицей
-
 	}
 
 	/**
 	 * Возвращает HTML представления класса по шорткоду
-	 */	
-    public function getHTML( $atts, $content='' )
-    {
-		
+	 */
+	public function getHTML( $atts, $content = '' ) {
+
 		// Проверяем аутентификацию пользователя, если нет - на авторизацию!
-		if ( ! is_user_logged_in() ) 
-		{
-		   auth_redirect();
-		}	
-		
+		if ( ! is_user_logged_in() ) {
+			auth_redirect();
+		}
+
 		// Получаем aтрибуты вызова и пропускаем их через фильтр shortcode_atts_$shortcode
 		// https://codex.wordpress.org/Function_Reference/shortcode_atts
-		$atts = shortcode_atts( array(
-			'foo' => 'no foo',
-			'baz' => 'default baz'
-		), $atts, self::SHORTCODE );	
-		
+		$atts = shortcode_atts(
+			array(
+				'foo' => 'no foo',
+				'baz' => 'default baz',
+			),
+			$atts,
+			self::SHORTCODE
+		);
+
 		// Загрузка CSS
 		wp_enqueue_style( 'handsontable' );
-		wp_enqueue_style( 'in-employee-reports-frontend' );		
-		
-		// Загрузка скриптов
-		wp_enqueue_script( 'in-employee-reports' );		
-		
-		
+		wp_enqueue_style( 'in-employee-reports-frontend' );
 
-		$year = date('Y');
+		// Загрузка скриптов
+		wp_enqueue_script( 'in-employee-reports' );
+
+		$year = date( 'Y' );
 		$html = <<<END_OF_HTML
 <section id="inerFrontend">
 	<div id="inerMessage">Сообщение</div>
@@ -172,5 +166,5 @@ class Frontend extends Base
 END_OF_HTML;
 
 		return $html;
-    }	
-}	
+	}
+}
--- source/classes/plugin.php
+++ source/classes/plugin.php
@@ -2,107 +2,112 @@
 /**
  * Основной класс плагина
  */
-namespace INER;
-class Plugin
-{
+namespace InEmployeeReports;
+
+class Plugin {
+
 	/**
 	 * Логи
+	 *
 	 * @static
 	 */
-	const ACTIVITY_LOG 	= 'activity.log';	
-	const ERROR_LOG 	= 'error.log';
-	
+	const ACTIVITY_LOG = 'activity.log';
+	const ERROR_LOG    = 'error.log';
+
 	/**
 	 * Путь к файлам плагина
+	 *
 	 * @var string
 	 */
 	public $path;
-	
+
 	/**
 	 * URL к файлам плагина
+	 *
 	 * @var string
 	 */
-	public $url;	
-		
+	public $url;
+
 	/**
 	 * Конструктор
 	 */
-	public function __construct( $pluginPath, $pluginURL )
-	{
+	public function __construct( $pluginPath, $pluginURL ) {
 		// Инициализация свойств
-		$this->path = $pluginPath;	// Путь к файлам плагина
-		$this->url = $pluginURL;	// URL к файлам плагина
-		
+		$this->path = $pluginPath;  // Путь к файлам плагина
+		$this->url  = $pluginURL;    // URL к файлам плагина
+
 		// Инициализация плагина по хуку init если пользователь авторизован
-		if ( wp_get_current_user() )
-		{
+		if ( wp_get_current_user() ) {
 			add_action( 'init', array( $this, 'init' ) );
 		}
-	}		
-	
+	}
+
 	/**
 	 * Экземпляр класса отчетов
-	 * @var INER\Report
+	 *
+	 * @var Report
 	 */
-	public $report;	
-	
+	public $report;
+
 	/**
 	 * Экземпляр класса фронтэнда
-	 * @var INER\Frontend
+	 *
+	 * @var Frontend
 	 */
-	public $frontend;		
-	
+	public $frontend;
+
 	/**
 	 * Экземпляр класса параметров
-	 * @var INER\Frontend
+	 *
+	 * @var Settings
 	 */
-	public $settings;	
-	
-	
+	public $settings;
+
+
 	/**
 	 * Инициализация плагина
 	 */
-	public function init()
-	{
-		$this->report 	= new Report( $this );
-		$this->frontend	= new Frontend( $this );
-		$this->settings	= new Settings( $this );
+	public function init() {
+		$this->report   = new Report( $this );
+		$this->frontend = new Frontend( $this );
+		$this->settings = new Settings( $this );
 	}
-	
+
 	/**
 	 * Запись логов
-	 * @param string	$log		Имя файла лога
-	 * @param string	$message	Сообщение для вывода
+	 *
+	 * @param string $log        Имя файла лога
+	 * @param string $message    Сообщение для вывода
 	 */
-	private function log( $log, $message )
-	{
+	private function log( $log, $message ) {
 		// Выводим логи только в режиме отладки
-		if ( ! WP_DEBUG )
+		if ( ! WP_DEBUG ) {
 			return;
-		
+		}
+
 		// Добавляем в сообщение дату, время и разделитель записей
 		$message = '[ ' . date( 'd.m.Y H:i:s' ) . ' ]' . PHP_EOL . $message . PHP_EOL . PHP_EOL;
-		
+
 		// Файл пишем в папку плагина
-		$log = $this->path . $log;
+		$log = $this->path . '..\\..\\log\\' . $log;
 		file_put_contents( $log, $message, FILE_APPEND );
 	}
-	
+
 	/**
 	 * Запись в лог активности
-	 * @param string	$message	Сообщение для вывода
+	 *
+	 * @param string $message    Сообщение для вывода
 	 */
-	public function activityLog( $message )
-	{	
+	public function activityLog( $message ) {
 		$this->log( self::ACTIVITY_LOG, $message );
 	}
-	
+
 	/**
 	 * Запись в лог ошибок
-	 * @param string	$message	Сообщение для вывода
+	 *
+	 * @param string $message    Сообщение для вывода
 	 */
-	public function errorLog( $message )
-	{	
+	public function errorLog( $message ) {
 		$this->log( self::ERROR_LOG, $message );
-	}	
+	}
 }
